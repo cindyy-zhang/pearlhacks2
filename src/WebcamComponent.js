85ee9cedd1df9b306f43ca0a75e608bbe46d40fe
Cindy Zhang <cindyzha@unc.edu>
2024-02-24 16:25:42 -0500

    webcam access

---
+++ src/WebcamComponent.js
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from 'react';
+
+function WebcamComponent() {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const getVideo = async () => {
+            try {
+                // access webcam
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                // if the videoRef is current and the stream is available, set the source object
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (err) {
+                // if there is no webcam
+                console.error("Error accessing the webcam: ", err);
+            }
+        };
+
+        getVideo();
+    }, [videoRef]); 
+
+    return (
+        <div>
+            <video
+              ref={videoRef}
+              autoPlay
+              playsInline
+              style={{
+                width: '40%',
+                height: '30%', 
+                transform: 'scaleX(-1)'
+              }}
+            />
+        </div>
+    );
+}
+
+export default WebcamComponent;
